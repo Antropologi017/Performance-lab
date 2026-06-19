4b0e5a2c9f811a8f95e629970f3fe245e7f2639f
Antrologi017 <Soier013@yandex.ru>
2026-06-20 01:59:20 +0300

    Добавил проверку последовательности и количество аргументов

--- task3/src/Task3.java
+++ task3/src/Task3.java
@@ -6,49 +6,56 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) throws Exception {
-        if (args.length < 3) return;
-        Map<String, String> statusMap = new HashMap<>();
-        Scanner valScanner = new Scanner(new File(args[0]));
-        String currentId = "";
-
-        while (valScanner.hasNextLine()) {
-            String line = valScanner.nextLine().trim();
-
-            if (line.contains("\"id\":")) {
-                currentId = line.replaceAll("[^0-9]", "");
-            }
-            if (line.contains("\"value\":") && !currentId.isEmpty()) {
-                String value = line.split("\"value\":")[1].replaceAll("[\", ]", "");
-                statusMap.put(currentId, value);
-                currentId = "";
-            }
+        if (args.length < 3) {
+            System.out.println("Введите 3 аргумента: values.json tests.json report.json");
+            return;
         }
-        valScanner.close();
+        if ("values.json".equals(args[0]) && "tests.json".equals(args[1]) && "report.json".equals(args[2])) {
+            Map<String, String> statusMap = new HashMap<>();
+            Scanner valScanner = new Scanner(new File(args[0]));
+            String currentId = "";
 
-        Scanner testScanner = new Scanner(new File(args[1]));
-        StringBuilder report = new StringBuilder();
-        String lastId = "";
+            while (valScanner.hasNextLine()) {
+                String line = valScanner.nextLine().trim();
 
-        while (testScanner.hasNextLine()) {
-            String line = testScanner.nextLine();
-            String trimmed = line.trim();
+                if (line.contains("\"id\":")) {
+                    currentId = line.replaceAll("[^0-9]", "");
+                }
+                if (line.contains("\"value\":") && !currentId.isEmpty()) {
+                    String value = line.split("\"value\":")[1].replaceAll("[\", ]", "");
+                    statusMap.put(currentId, value);
+                    currentId = "";
+                }
+            }
+            valScanner.close();
 
-            if (trimmed.contains("\"id\":")) {
-                lastId = trimmed.replaceAll("[^0-9]", "");
+            Scanner testScanner = new Scanner(new File(args[1]));
+            StringBuilder report = new StringBuilder();
+            String lastId = "";
+
+            while (testScanner.hasNextLine()) {
+                String line = testScanner.nextLine();
+                String trimmed = line.trim();
+
+                if (trimmed.contains("\"id\":")) {
+                    lastId = trimmed.replaceAll("[^0-9]", "");
+                }
+                if (trimmed.matches(".*\"value\":\\s*\"\"*.") && !lastId.isEmpty() && statusMap.containsKey(lastId)) {
+                    line = line.replace("\"\"", "\"" + statusMap.get(lastId) + "\"");
+                }
+                if (trimmed.equals("}") || trimmed.equals("},") || trimmed.equals("}]")) {
+                    lastId = "";
+                }
+                report.append(line).append("\n");
             }
-            if (trimmed.matches(".*\"value\":\\s*\"\"*.") && !lastId.isEmpty() && statusMap.containsKey(lastId)) {
-                line = line.replace("\"\"", "\"" + statusMap.get(lastId) + "\"");
-            }
-            if (trimmed.equals("}") || trimmed.equals("},") || trimmed.equals("}]")) {
-                lastId = "";
-            }
-            report.append(line).append("\n");
+            testScanner.close();
+
+            FileWriter writer = new FileWriter(args[2]);
+            writer.write(report.toString().trim());
+            writer.close();
+            System.out.println("Отчет " + args[2] + " готов!");
+        }else {
+            System.out.println("Неверный порядок аргументов, должен быть: values.json tests.json report.json");
         }
-        testScanner.close();
-
-        FileWriter writer = new FileWriter(args[2]);
-        writer.write(report.toString().trim());
-        writer.close();
-        System.out.println("Отчет report.json готов!");
     }
 }
